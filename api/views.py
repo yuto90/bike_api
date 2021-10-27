3deead8a0d65fe5f4082db1c13bc1ed9f3029070
yuto90 <solidusgako4087@gmail.com>
2021-10-27 20:54:18 +0900

    viewを記述

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from api import serializers
+from .models import Maker, Bike, UserProfile
+from rest_framework import generics
+from rest_framework import viewsets
 
-# Create your views here.
+
+# ListAPIView メソッド：GET 一覧取得
+class BikeListView(generics.ListAPIView):
+    queryset = Bike.objects.all()
+    serializer_class = serializers.BikeSerializer
+
+# ListAPIView メソッド：GET 一覧取得
+
+
+class MakerListView(generics.ListAPIView):
+    queryset = Maker.objects.all()
+    serializer_class = serializers.MakerSerializer
+
+
+# ModelViewSet　1つのエンドポイントでmodelに紐付いたCRUD処理を実装してくれる
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = serializers.UserProfileSerializer
